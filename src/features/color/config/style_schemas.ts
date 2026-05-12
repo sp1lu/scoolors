485491b14a2c8f176a5b7870cf5cd7aa8dedf43e
sp1lu <davide.rivolta@yahoo.it>
2026-05-12 23:38:24 +0200

    Fixed bug in style schemas

--- src/features/color/config/style_schemas.ts
+++ src/features/color/config/style_schemas.ts
@@ -2,8 +2,8 @@ import type { StyleSchema } from '../types'
 
 export const STYLE_SCHEMAS: StyleSchema = new Map([
     ['css', `\t--{{key}}: {{value}};\n`],
-    ['scss', `$${'{'}{key}}: {{value}};\n`],
-    ['sass', `$${'{'}{key}}: {{value}}\n`],
+    ['scss', '${{key}}: {{value}};\n'],
+    ['sass', '${{key}}: {{value}}\n'],
     ['less', `@{{key}}: {{value}};\n`],
     ['json', `\t"{{key}}": "{{value}}",\n`],
 ]);
